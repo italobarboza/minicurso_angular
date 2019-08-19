7f602cb2f462a1f0092b69882f0dfe47644a2c5e
Ítalo Barboza <italo@italobarboza.com.br>
2019-08-18 23:59:30 -0300

    feat: module imports
    hammerjs, angular-material (animations) and flex-layout imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './move/components/movie-list/movie-list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -12,7 +14,9 @@ import { MovieListComponent } from './move/components/movie-list/movie-list.comp
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FlexLayoutModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
