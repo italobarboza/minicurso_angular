8466c6484edb8d917f80d5c3fe139524c36da7ca
Ítalo Barboza <italo@italobarboza.com.br>
2019-08-20 19:48:50 -0300

    refactor: processing indication

--- src/app/movie/components/movie-list/movie-list.component.ts
+++ src/app/movie/components/movie-list/movie-list.component.ts
@@ -33,9 +33,9 @@ export class MovieListComponent implements OnInit {
         if (this.processing === false && this.endPaginate === false) {
             const target = $event.target.documentElement;
             const scrollPercent = ((target.clientHeight + target.scrollTop) / target.scrollHeight) * 100;
-
             if (scrollPercent > 95) {
                 this.page++;
+                this.processing = true;
                 this.movieService.getPopular(this.page).then((res) => this.inserMovies(res.data.results));
             }
         }
