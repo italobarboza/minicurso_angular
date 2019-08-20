08ce45ce0b22bce2fd157fcf6351e81005069f3d
Ítalo Barboza <italo@italobarboza.com.br>
2019-08-20 19:23:46 -0300

    refactor: movie poster functionality

--- src/app/movie/components/movie-poster/movie-poster.component.ts
+++ src/app/movie/components/movie-poster/movie-poster.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { environment as env } from '../../../../environments/environment';
 
 @Component({
-  selector: 'app-movie-poster',
-  templateUrl: './movie-poster.component.html',
-  styleUrls: ['./movie-poster.component.scss']
+    selector: 'app-movie-poster',
+    templateUrl: './movie-poster.component.html',
+    styleUrls: ['./movie-poster.component.scss']
 })
-export class MoviePosterComponent implements OnInit {
+export class MoviePosterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    mediaQuerySubscription: Subscription;
+    activeMediaQuery: string;
 
-  ngOnInit() {
-  }
+    @Input() image: string;
+    @Input() imageXs: string;
+
+    constructor(
+        private mediaObserver: MediaObserver
+    ) {
+        this.mediaQuerySubscription = this.mediaObserver.media$.subscribe(
+            (change: MediaChange) => {
+                this.activeMediaQuery = change.mqAlias
+            }
+        );
+    }
+
+    ngOnInit() {
+        this.imageXs = `${env.api.imageEndpoint}/${this.imageXs}`;
+        this.image = `${env.api.imageEndpoint}/${this.image}`;
+    }
+
+    ngOnDestroy() {
+        if (this.mediaQuerySubscription) {
+            this.mediaQuerySubscription.unsubscribe();
+        }
+    }
 
 }
